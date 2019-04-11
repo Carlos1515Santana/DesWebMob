fbc7856ac88e214c12a94477bedc47ad46933d51
Carlos Sergio Santana Santos <carlossergio1515@gmail.com>
2019-04-10 23:08:24 -0300

    Aula06-05

---
+++ app/src/main/java/br/usjt/ciclodevidagpsemapas/ListaLocaisActivity.java
@@ -0,0 +1,30 @@
+
+
+package br.usjt.ciclodevidagpsemapas;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ListaLocaisActivity extends AppCompatActivity {
+
+    private LocalizacaoDAO localizacaoDAO;
+    private RecyclerView locationsRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_locais);
+        localizacaoDAO = new  LocalizacaoDAO(this);
+        ArrayList<Localizacao> localizacoes = (ArrayList<Localizacao>) localizacaoDAO.lista();
+        locationsRecyclerView = findViewById(R.id.localizacoesRecyclerView);
+        locationsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        LocaisRecyclerViewAdapter adapter = new LocaisRecyclerViewAdapter(localizacoes);
+        locationsRecyclerView.setAdapter(adapter);
+
+    }
+}
